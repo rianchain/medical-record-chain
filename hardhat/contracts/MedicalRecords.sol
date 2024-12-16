d4cddac0eb9ef3dfd906bee44531ffdf72011770
etherian <rianziwalker@gmail.com>
2024-12-16 20:06:44 +0700

    added a new struct data

--- hardhat/contracts/MedicalRecords.sol
+++ hardhat/contracts/MedicalRecords.sol
@@ -3,6 +3,12 @@ pragma solidity ^0.8.24;
 
 contract MedicalRecords {
 
-    /// Variable state untuk menampung data pasien
+    /// Variable state untuk menampung data pasien:
+
+    struct Patient {
+        string nama;
+        string alamat;
+        string gender;
+    }
 
 }
