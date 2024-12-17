156ba038f13103942a71e21bf64a7a4b715b08f1
etherian <rianziwalker@gmail.com>
2024-12-17 21:02:01 +0700

    =

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,9 +1,18 @@
+require("dotenv").config();
+require("@nomicfoundation/hardhat-ethers");
 require("@nomicfoundation/hardhat-toolbox");
+
+const { API_URL, PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
-  settings: {
-    viaIR: true,
+  defaultNetwork: "optimismSepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
   },
 };
