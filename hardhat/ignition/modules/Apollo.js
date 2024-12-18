fa92400aabf94ca91f53bbdccaa866dbe65cb186
etherian <rianziwalker@gmail.com>
2024-12-18 21:51:25 +0700

    =

--- hardhat/ignition/modules/Apollo.js
+++ hardhat/ignition/modules/Apollo.js
@@ -3,4 +3,10 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/mdoules");
 module.exports = buildModule("Appolo", (m) => {
   // definisikan contract itu
   const medicalRecords = m.contract("MedicalRecords", []);
+
+  // memanggil constructor untuk mendeploy contract
+  m.call(medicalRecords, "constructor", []);
+
+  // mengembalikan/return contoh contract
+  returrn { medicalRecords };
 });
